dca7a7ad4e1bc06043ce96d414a71b1777c4c7de
chanul94 <cksdnfwkd@naver.com>
2020-02-02 18:40:14 +0900

    commit test

--- hello.py
+++ hello.py
@@ -11,3 +11,16 @@ def count_list(a_list) :
 
 print(count_list(a))
 
+
+
+
+a = ['사과', '감', '감', '배', '포도', '포도', '딸기', '포도', '감', '수박', '딸기']
+
+def count_list(a_list) :
+    result = {}
+    for element in a_list:
+        if element in result:
+            result[element] +=1
+        else:
+
+print(count_list(a))
